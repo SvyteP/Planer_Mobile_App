d33793e3073833c5b8f99edda7c601a007e4c142
Max <Max161161161@mail.ru>
2023-04-14 15:33:20 +0300

    del test comment

--- app/src/main/java/com/example/planer_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/planer_mobile_app/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//уета
-        setContentView(R.layout.activity_main);//хуй залупа сперма пирожки
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
