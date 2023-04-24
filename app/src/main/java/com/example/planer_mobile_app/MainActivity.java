9fa196d12024178b4424f79999f9d3e09abf999b
Max <Max161161161@mail.ru>
2023-04-24 17:21:51 +0300

    Add DataBase FireBase№3

--- app/src/main/java/com/example/planer_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/planer_mobile_app/MainActivity.java
@@ -6,13 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mRefDB;//Переменная для адреса БД
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//хуй залупа сперма пирожки
-
+        setContentView(R.layout.activity_main);
+        mRefDB = FirebaseDatabase.getInstance().getReference();//Запись адреза БД
     }
+
     public void toRegistration(View reg){
         Intent toReg = new Intent(MainActivity.this, Registration.class);
         startActivity(toReg);
